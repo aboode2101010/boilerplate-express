7478c97a1f714a1e571fda2f09ed9898b94331c3
aboode2101010 <112402602+aboode2101010@users.noreply.github.com>
2024-04-28 10:42:14 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,7 +3,16 @@ let app = express();
 
 
 
+app.listen(3000)
 
+console.log(__dirname)
+
+app.get('/hello', (request, response) => {
+    response.send("Hello Express")
+app.get('/hello', (request, response) => {
+    response.sendFile(__dirname +'/index.html')
+
+})
 
 
 
