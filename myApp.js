e3a14d7b13fe423ab9bf67251983b1d30b71a786
aboode2101010 <112402602+aboode2101010@users.noreply.github.com>
2024-05-03 14:17:12 +0300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,5 +1,23 @@
-let express = require('express');
-let app = express();
+var express = require("express");
+var app = express();
+
+
+
+// Challenge 2
+app.get("/", (request, respond) => {
+	response.sendFile=(__dirname + "/views/index.html");
+	
+})
+
+
+// Challenge 1
+// app.get("/", (request, respond) {
+// 	response.send("Hello Express");
+// })
+
+
+// Exercise
+// console.log("Hello World");
 
 
 
